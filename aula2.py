7f8e387e60a2c565d63dad0d5451a08abe423bdd
Lara Ferreira da Silva <laraferreirasilva50@gmail.com>
2026-08-20 11:29:48 -0300

    Fix input function for second number in calculator
    inclui o input na linha 7

--- aula2.py
+++ aula2.py
@@ -4,7 +4,7 @@ print("=== Calculadora ===")
 
 num1 = float(input("Digite o primeiro número: "))
 operacao = input("Digite a operação (+, -, *, /): ")
-num2 = float(("Digite o segundo número: "))
+num2 = float(input("Digite o segundo número: "))
 
 if operacao == "+":
     resultado = num1 + num2
